58fc14a48f84389f12046b56fbd82327603f8eea
Prince <2023ugec056@nitjsr.ac.in>
2026-04-03 16:21:10 +0530

    home page update

--- backend/index.js
+++ backend/index.js
@@ -26,9 +26,7 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(cookieParser())
 
-app.get("/",(req,res)=>{
-    res.send("hello prince always be happy")
-})
+
 
 //
 app.use("/api/user", userRoute);
